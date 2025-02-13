d4ea764aa77604068ed01b6fe0709f8237ca30f6
Viktoia Polikanova <v.s.polikanova@gmail.com>
2025-02-13 15:55:39 +0700

    start higher

--- handlers.py
+++ handlers.py
@@ -16,6 +16,31 @@ router_main = Router()
 tz_novosibirsk = pytz.timezone('Asia/Novosibirsk')
 
 
+@router_main.message(F.text == "/start")
+async def send_welcome(message: types.Message):
+    user = message.from_user
+    user_id = user.id
+    username = user.username
+    key = message.text.split(' ')[1] if len(
+        message.text.split(' ')) > 1 else ""
+
+    logger.info(
+        f'User {username} (ID: {user_id}) started the bot with key: {key}')
+
+    payload = {"key": key, "username": username, "id": user_id}
+    response_data = await send_request("add_user", payload)
+    logger.info(f"Ответ от сервера авторизации: {response_data}")
+
+    if 'error' in response_data and 'error_msg' in response_data:
+        await message.answer(f"Ошибка авторизации: {response_data['error_msg']}")
+        logger.error(
+            f'Authorization error for user {username}: {response_data["error_msg"]}')
+    else:
+        await message.answer("Вы успешно авторизованы!")
+        logger.info(
+            f'User {username} (ID: {user_id}) authorized successfully.')
+
+
 @router_main.message(F.text == "Список рейсов на сегодня")
 async def handle_button1(message: types.Message, bot: Bot):
     from keyboard import send_routes
@@ -51,30 +76,6 @@ async def handle_button2(message: types.Message, bot: Bot):
 
 # Обработчик команды /start
 
-
-@router_main.message(F.text == "/start")
-async def send_welcome(message: types.Message):
-    user = message.from_user
-    user_id = user.id
-    username = user.username
-    key = message.text.split(' ')[1] if len(
-        message.text.split(' ')) > 1 else ""
-
-    logger.info(
-        f'User {username} (ID: {user_id}) started the bot with key: {key}')
-
-    payload = {"key": key, "username": username, "id": user_id}
-    response_data = await send_request("add_user", payload)
-    logger.info(f"Ответ от сервера авторизации: {response_data}")
-
-    if 'error' in response_data and 'error_msg' in response_data:
-        await message.answer(f"Ошибка авторизации: {response_data['error_msg']}")
-        logger.error(
-            f'Authorization error for user {username}: {response_data["error_msg"]}')
-    else:
-        await message.answer("Вы успешно авторизованы!")
-        logger.info(
-            f'User {username} (ID: {user_id}) authorized successfully.')
 
 # Обработчик текстовых сообщений
 
