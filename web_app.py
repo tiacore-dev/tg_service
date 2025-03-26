be9e61bd76e76a726080d854a50e404c6c7d5861
Viktoria <v.s.polikanova@gmail.com>
2025-03-26 14:49:10 +0700

    attachments

--- web_app.py
+++ web_app.py
@@ -1,7 +1,10 @@
 import logging
 import os
+from io import BytesIO
+import base64
 from dotenv import load_dotenv
 from aiohttp import web, ClientSession
+import aiohttp
 
 logger = logging.getLogger(__name__)
 # Aiohttp маршрут для получения сообщений
@@ -12,11 +15,35 @@ TG_API_TOKEN = os.getenv('TG_API_TOKEN')
 
 async def handle_post_request(request):
     json_data = await request.json()
-    data = {"chat_id": json_data['userid'], "text": json_data['text']}
-
+    chat_id = json_data.get('userId')
+    text = json_data.get('text')
+    attachments = json_data.get('attachments')
+    if not text:
+        text = ""
+    data = {"chat_id": chat_id, "text": text}
+    # Отправляем сначала текст
     async with ClientSession() as session:
         async with session.post(f'https://api.telegram.org/bot{TG_API_TOKEN}/sendMessage', json=data) as response:
             result = await response.json()
+        if attachments:
+            for attachment in attachments:
+                file_name = attachment['fileName']
+                base64_data = attachment['base64Data']
+                try:
+                    file_bytes = base64.b64decode(base64_data)
+                    file_obj = BytesIO(file_bytes)
+                    file_obj.name = file_name
 
-    logger.info(f'Sent message to Telegram: {data}')
-    return web.json_response(result)
+                    send_doc_url = f"https://api.telegram.org/bot{TG_API_TOKEN}/sendDocument"
+                    form = aiohttp.FormData()
+                    form.add_field("chat_id", str(chat_id))
+                    form.add_field("document", file_obj, filename=file_name)
+
+                    async with session.post(send_doc_url, data=form) as resp:
+                        result = await resp.json()
+                        logger.info(
+                            f"📎 Sent file {file_name} to Telegram: {result}")
+                except Exception as e:
+                    logger.error(f"❌ Failed to send file {file_name}: {e}")
+
+    return web.json_response({"status": "ok"})
